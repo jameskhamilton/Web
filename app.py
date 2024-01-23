704ab8a60114ec88bc661c81a3d4581591a3bd67
jameskhamilton <142740578+jameskhamilton@users.noreply.github.com>
2024-01-23 18:42:10 +0000

    hello world

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, jsonify, render_template
-from flask_cors import CORS
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
