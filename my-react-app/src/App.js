36dd9f1fe588e123ae42196c6e23da33a1378b50
jameskhamilton <142740578+jameskhamilton@users.noreply.github.com>
2024-01-23 18:01:57 +0000

    adding react framework

---
+++ my-react-app/src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+// src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+    </div>
+  );
+}
+
+export default App;
